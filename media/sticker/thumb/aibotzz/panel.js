0ac59f51886c0c005816e3e65cc3f19a6064bdf5
ManuDevBot <manugithub3@gmail.com>
2024-08-09 17:47:29 +0530

    𝗠𝗔𝗡𝗨-𝗠𝗗

---
+++ media/sticker/thumb/aibotzz/panel.js
@@ -0,0 +1,8 @@
+const domain = 'https://vemzslot777.dikahosting.my.id' //isi domain mu
+const apikey = 'ptla_Y1A5lcJAt6tuf6bwX5anQubDm1xCRfKY5vb6IKtfDzl' //apikey plta
+const capikey = 'ptlc_SZvNezz5LbXqw1UxA0i90qbxpb3EGRaV9MDyUkCzpUH' //apikey pltc
+const eggsnya = '15' 
+const location = '1' 
+
+module.exports = { domain, apikey, capikey, eggsnya, location }
+
